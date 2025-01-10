a9e2e77c718dd694d67ac6e253231c6263ff923b
Jhon Florez <134735225+Eddyzzero@users.noreply.github.com>
2025-01-10 14:19:56 +0100

    components creation for search bar

---
+++ src/components/seachBar.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <!-- input de recherche -->
+   <div class="bg-sky-950 p-8 space-x-5 flex-col">
+    <label class="text-xl text-blue-300" for="site-search "> chercher une ville :</label>
+    <input class=" rounded bg-slate-300" type="search" id="site-search">
+   </div>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+      title : "search bar",
+      
+
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import './css/input.css'
+import App from './components/seachBar.vue'
+import './css/output.css'
 
 createApp(App).mount('#app')
